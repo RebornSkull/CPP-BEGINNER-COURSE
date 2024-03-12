eb533f878d01b2810cadc8360a4f71686b4c3132
SKULL <160460834+RebornSkull@users.noreply.github.com>
2024-03-12 00:24:07 -0500

    commit

---
+++ CPP Beginner Course/UserInput/UserInput.cpp
@@ -0,0 +1,16 @@
+#include <iostream?
+
+// cout << (insertion operator)
+// cin >> (extraction operator)
+
+int main(){
+
+    std::string name;
+
+    std::cout << "What's your name?: ";
+    std::cin >> name;
+
+    std::cout << "Hello " << name;
+
+    return 0;
+}
